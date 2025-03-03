53ee32e4d096a062e8df68f88d740a2a9779f8e8
Matthew Gaim <matthewgaim@gmail.com>
2025-03-02 21:36:38 -0800

    be able to save 0 allowed channels

--- components/channel-selector.tsx
+++ components/channel-selector.tsx
@@ -223,7 +223,7 @@ export function ChannelSelector({
 
       {/* Save button */}
       <div className="flex justify-end">
-        <Button onClick={handleSave} disabled={isPending || selectedChannels.length === 0} className="gap-2">
+        <Button onClick={handleSave} disabled={isPending || preSelected === selectedChannels} className="gap-2">
           {isPending ? (
             <>
               <Loader2 className="h-4 w-4 animate-spin" />
