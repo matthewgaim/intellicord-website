c47e99869cdcef62c84917e1a47ecb47a7f0842f
Matthew Gaim <matthewgaim@gmail.com>
2025-03-04 12:32:14 -0800

    dont let unregistered ppl invite bot

--- app/actions/dashboard-info.ts
+++ app/actions/dashboard-info.ts
@@ -3,16 +3,16 @@
 import { cookies } from "next/headers";
 
 export async function getBotInviteUrl() {
-  const { DISCORD_CLIENT_ID } = process.env;
+  const { DISCORD_CLIENT_ID, DISCORD_REDIRECT_URI } = process.env;
 
-  if (!DISCORD_CLIENT_ID) {
-    throw new Error("Missing DISCORD_CLIENT_ID in environment variables");
+  if (!DISCORD_CLIENT_ID || !DISCORD_REDIRECT_URI) {
+    throw new Error("Missing environment variables");
   }
 
   const permissions = "8"; // Admin
   const scopes = "bot applications.commands identify email guilds connections guilds.join";
 
-  return `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&permissions=${permissions}&scope=${encodeURIComponent(scopes)}`;
+  return `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&permissions=${permissions}&scope=${encodeURIComponent(scopes)}&response_type=code&redirect_uri=${encodeURIComponent(DISCORD_REDIRECT_URI)}`;
 }
 
 export async function getJoinedServers(){
