7d975973d9a20b1c6303750c30cb49360b58a38f
Matthew Gaim <matthewgaim@gmail.com>
2025-10-30 22:08:09 -0700

    movin on to plain html

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+console.log("We're looking at main.ts!");
+
+const hostname = window.location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "https://intellicord.senarado.com";
+
+const scope = 'identify email guilds';
+const DISCORD_CLIENT_ID = window.location.hostname === 'localhost'
+    ? '1343462968142200932'
+    : '1339021294234239028';
+const DISCORD_REDIRECT_URI = `${hostname}/src/auth.html`;
+const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${encodeURIComponent(DISCORD_CLIENT_ID)}&redirect_uri=${encodeURIComponent(DISCORD_REDIRECT_URI)}&response_type=code&scope=${encodeURIComponent(scope)}`;
+
+window.addEventListener("DOMContentLoaded", () => {
+    const loginBtn = document.getElementById("login");
+    const addToServerBtn = document.getElementById("add-to-server");
+
+    if (addToServerBtn instanceof HTMLAnchorElement) {
+        addToServerBtn.href = authUrl;
+    }
+    if (loginBtn instanceof HTMLAnchorElement) {
+        loginBtn.href = authUrl;
+    }
+});
+
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'tailwindcss'
+
+export default defineConfig({
+  content: ['./src/**/*.{html,js,ts}'],
+  theme: {
+    extend: {},
+  },
+})
