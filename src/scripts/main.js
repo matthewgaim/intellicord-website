37670a00a00165404107e461dba5c88841327499
Matthew Gaim <matthewgaim@gmail.com>
2025-10-30 23:09:02 -0700

    file structs changed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,7 +9,7 @@ const DISCORD_CLIENT_ID = window.location.hostname === 'localhost'
     ? '1343462968142200932'
     : '1339021294234239028';
 const DISCORD_REDIRECT_URI = `${hostname}/auth.html`;
-const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${encodeURIComponent(DISCORD_CLIENT_ID)}&redirect_uri=${encodeURIComponent(DISCORD_REDIRECT_URI)}&response_type=code&scope=${encodeURIComponent(scope)}`;
+const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&redirect_uri=${encodeURIComponent(DISCORD_REDIRECT_URI)}&response_type=code&scope=${encodeURIComponent(scope)}`;
 
 window.addEventListener("DOMContentLoaded", () => {
     const loginBtn = document.getElementById("login");
