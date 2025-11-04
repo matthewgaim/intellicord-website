df2cc4a596513be597e6d3d3db1169009892bf6b
Matthew Gaim <matthewgaim@gmail.com>
2025-11-03 22:16:52 -0800

    start servers.html

--- src/scripts/dashboard.js
+++ src/scripts/dashboard.js
@@ -1,3 +1,26 @@
+async function filesAllServers() {
+  const API_URL = window.location.hostname === 'localhost'
+    ? 'http://localhost:5500'
+    : 'https://intellicord-api.senarado.com';
+
+  try {
+    const res = await fetch(`${API_URL}/analytics/files-all-servers`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include"
+    });
+
+    if (!res.ok) throw new Error("Login failed");
+    const data = await res.json();
+
+    console.log("Weekly File Data:", data);
+  } catch (err) {
+    console.error(err);
+    window.location.href = `/`;
+  }
+}
+filesAllServers();
+
 window.addEventListener("DOMContentLoaded", () => {
   const ctx = document.getElementById("myChart");
   if (!ctx) return;
---
+++ src/scripts/servers.js
@@ -0,0 +1,20 @@
+const hostname = window.location.hostname === "localhost"
+    ? "http://localhost:5500"
+    : "https://intellicord.senarado.com";
+const DISCORD_CLIENT_ID = window.location.hostname === 'localhost'
+    ? '1343462968142200932'
+    : '1339021294234239028';
+const DISCORD_REDIRECT_URI = `${hostname}/auth.html`;
+const API_URL = window.location.hostname === 'localhost'
+    ? 'http://localhost:5500'
+    : 'https://intellicord-api.senarado.com';
+
+const permissions = "8"; // Admin
+const scopes = "bot applications.commands identify email guilds connections guilds.join";
+
+const BOT_INVITE_URL = `https://discord.com/api/oauth2/authorize?client_id=${DISCORD_CLIENT_ID}&permissions=${permissions}&scope=${encodeURIComponent(scopes)}&response_type=code&redirect_uri=${encodeURIComponent(DISCORD_REDIRECT_URI)}`;
+
+window.addEventListener("DOMContentLoaded", () => {
+    const inviteBotButton = document.getElementById("invite-bot-button");
+    inviteBotButton.href = BOT_INVITE_URL;
+});
