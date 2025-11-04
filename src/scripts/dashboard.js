8a9b31b1e69355ef312556b3763f32d3955b4134
Matthew Gaim <matthewgaim@gmail.com>
2025-11-03 22:37:15 -0800

    show data on /dashboard

--- src/scripts/dashboard.js
+++ src/scripts/dashboard.js
@@ -1,7 +1,8 @@
-async function filesAllServers() {
-  const API_URL = window.location.hostname === 'localhost'
+const API_URL = window.location.hostname === 'localhost'
     ? 'http://localhost:5500'
     : 'https://intellicord-api.senarado.com';
+
+async function filesAllServers() {
 
   try {
     const res = await fetch(`${API_URL}/analytics/files-all-servers`, {
@@ -12,45 +13,49 @@ async function filesAllServers() {
 
     if (!res.ok) throw new Error("Login failed");
     const data = await res.json();
+    const file_details = data.file_details;
+    const files_analyzed = data.files_analyzed;
+    const total_messages_count = data.total_messages_count;
+    let dates = files_analyzed.map((file) => file.date);
+    let daily_file_amount = files_analyzed.map((file) => file.amount);
+    
+    const ctx = document.getElementById("myChart");
+    if (!ctx) return;
 
-    console.log("Weekly File Data:", data);
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: dates,
+        datasets: [{
+          label: 'Files Uploaded',
+          data: daily_file_amount,
+          borderWidth: 2,
+          backgroundColor: 'rgb(0,0,0)',
+          borderColor: 'rgb(239, 163, 0)',
+          borderRadius: 6,
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: { beginAtZero: true }
+        },
+        plugins: {
+          legend: {
+            labels: {
+              color: '#1e293b',
+              font: { size: 14 }
+            }
+          }
+        }
+      }
+    });
   } catch (err) {
     console.error(err);
     window.location.href = `/`;
   }
 }
-filesAllServers();
 
 window.addEventListener("DOMContentLoaded", () => {
-  const ctx = document.getElementById("myChart");
-  if (!ctx) return;
-    console.log(document.cookie);
-  new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      datasets: [{
-        label: 'Files Uploaded',
-        data: [12, 19, 3, 5, 2, 3, 8],
-        borderWidth: 2,
-        backgroundColor: 'rgb(0,0,0)',
-        borderColor: 'rgb(239, 163, 0)',
-        borderRadius: 6,
-      }]
-    },
-    options: {
-      responsive: true,
-      scales: {
-        y: { beginAtZero: true }
-      },
-      plugins: {
-        legend: {
-          labels: {
-            color: '#1e293b',
-            font: { size: 14 }
-          }
-        }
-      }
-    }
-  });
+  filesAllServers();
 });
